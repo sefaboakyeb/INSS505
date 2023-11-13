9b2b00a4a08b944b01f97e3f9482896560c70173
sefab <sefaboakyeb1023@students.bowiestate.edu>
2023-11-13 17:57:08 -0500

    lab1-2

---
+++ class 1.py
@@ -0,0 +1,13 @@
+input1 = 5
+input2 = 6
+output = input1 + input2
+print(input1,input2,output)
+firstinput = 5
+secondinput = 6
+thirdinput = 7
+output = firstinput * secondinput * thirdinput
+print(output)
+fourthinput = 8
+output1 = firstinput + secondinput + thirdinput + fourthinput
+output2 = firstinput * secondinput * thirdinput * fourthinput
+print(output1,output2)
